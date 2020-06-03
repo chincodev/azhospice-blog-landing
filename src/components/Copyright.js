f68816e371803ddebe1ec10926c25277e03266d6
anthony952 <anthonymedina958@hotmail.com>
2020-06-03 03:22:47 -0400

    Website running without blog

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,16 +3,16 @@ import React from 'react'
 
 const Copyright = () => (
     <>
-        <div className="copy-w3pvt">
-	        <div className="container py-3">
-	    		<div className="row">
-	    			<div className="col-lg-12 w3ls_footer_grid1_left text-lg-center text-center">
+        <div class="copy-w3pvt">
+	        <div class="container py-3">
+	    		<div class="row">
+	    			<div class="col-lg-12 w3ls_footer_grid1_left text-lg-center text-center">
 	    				<p>&copy; 2020 A-Z Hospice Inc.</p>
 	    			</div>
 	    		</div>
 	    	</div>
 	    </div>
-        <button type="button" className="move-top text-center"></button>
+        <a href="#home" class="move-top text-center"></a>
     </>
 )
 export default Copyright
