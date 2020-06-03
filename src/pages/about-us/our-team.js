7a756dbf78d701a901f9b2ffdd66e8f99c59da5c
anthony952 <anthonymedina958@hotmail.com>
2020-06-02 22:57:30 -0400

    Website running without blog

--- src/pages/about-us/our-team.js
+++ src/pages/about-us/our-team.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import Container from '../../components/Container'
 import SEO from '../../components/SEO'
+import { Link } from "gatsby"
 
 const ServicesWeProvide = () => {
     return (
