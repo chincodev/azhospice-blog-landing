123580b80fb30c3e98d78f363faabd244c654517
anthony952 <anthonymedina958@hotmail.com>
2020-06-04 01:05:31 -0400

    Finished

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -28,13 +28,13 @@ const ShareButtons = ({title,tags,slug}) => {
         <div >
             <small>Share in your social networks</small>
             <div>
-              <FacebookShareButton url={baseUrl+'/'+slug}>
+              <FacebookShareButton url={baseUrl+'/blug/'+slug}>
                   <span className="fa share-btn fa-facebook mr-2"></span>
               </FacebookShareButton>
-              <TwitterShareButton url={baseUrl+'/'+slug} title={title} via={'@a-zhospice'} hashtags={tags} >
+              <TwitterShareButton url={baseUrl+'/blug/'+slug} title={title} via={'@a-zhospice'} hashtags={tags} >
                   <span className="fa share-btn fa-twitter mr-2"></span> 
               </TwitterShareButton> 
-              <LinkedinShareButton url={baseUrl+'/'+slug} title={title} >
+              <LinkedinShareButton url={baseUrl+'/blug/'+slug} title={title} >
                   <span className="fa share-btn fa-linkedin mr-2"></span>
               </LinkedinShareButton>
             </div>
