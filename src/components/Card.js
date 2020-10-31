338e19dc858bd3d797d9c8eedb932e0882d44893
unknown <anthonymedina958@gmail.com>
2020-10-31 15:45:35 -0400

    added posts to index

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,15 +9,15 @@ const Card = ({ slug, heroImage, title, publishDate, body, ...props }) => {
     {
     
       heroImage && body && (
-        <article class="col-sm-6 col-xl-4 ">
+        <article class="col-sm-6 col-xl-4 d-flex">
             <Link to={`${props.basePath}/${slug}/`}>
-                <div class="card post-card d-flex flex-column">
+                <div class="card post-card flex-column flex-fill">
                     <Img  class="card-img-top" fluid={heroImage.fluid} alt="And this isn't my nose. This is a false one." />
-                    <div class="card-body d-flex flex-column">
+                    <div class="card-body flex-column">
                     <small class="d-block text-muted" style={{paddingBottom:"15px"}}>⏱️ {body.childMarkdownRemark.timeToRead} min read</small>
                       <h4 class="card-title" style={{color:"#212121"}}>{title}</h4>
                       <div class="text-muted text-preview" dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.excerpt}}></div>
-                      <div class="d-flex align-items-center pt-5 mt-auto">
+                      <div class="align-items-center pt-5 mt-auto">
                           <small class="d-block text-muted">📅 {publishDate}</small>
                       </div>
                     </div>
