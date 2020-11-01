d373aa5380318e396389fe64228b009d846ecde4
unknown <anthonymedina958@gmail.com>
2020-11-01 03:47:08 -0400

    added posts to index

--- src/components/PostLinks.js
+++ src/components/PostLinks.js
@@ -9,12 +9,12 @@ const PostLinks = props => {
     <div>
       <div class="post-links">
         {props.previous && (
-          <Link to={`${props.basePath}/${props.previous.slug}/`}>
+          <Link to={`${props.basePath}/${props.previous.slug}/`} className="page-buttons">
             &#8592; Prev
           </Link>
         )}
         {props.next && (
-          <Link to={`${props.basePath}/${props.next.slug}/`}>
+          <Link to={`${props.basePath}/${props.next.slug}/`} className="page-buttons">
             Next &#8594;
           </Link>
         )}
