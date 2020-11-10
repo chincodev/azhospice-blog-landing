b392a074e3d5a005b3a81f588c04f52a2681637e
unknown <anthonymedina958@gmail.com>
2020-11-10 06:36:34 -0400

    added organization info to tags

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,7 @@ const Card = ({ slug, heroImage, title, publishDate, body, ...props }) => {
             {console.log(heroImage)}
             <Link to={`${props.basePath}/${slug}/`} className='card post-card flex-column flex-fill' >
               <div className="top-sec">
-                <img loading="lazy" class="card-img-top" fluid={heroImage.fluid} alt="And this isn't my nose. This is a false one." />
+                <img class="card-img-top" fluid={heroImage.fluid} alt="And this isn't my nose. This is a false one." />
                   <div class="card-body flex-column">
                   <small class="d-block text-muted" style={{paddingBottom:"15px"}}>⏱️ {body.childMarkdownRemark.timeToRead} min read</small>
                     <h4 class="card-title" style={{color:"#212121"}}>{title}</h4>
